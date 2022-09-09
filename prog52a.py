fce2d9f334f894746fd23286a0bd37ea0c913a6d
granja-c <112727067+granja-c@users.noreply.github.com>
2022-09-09 19:19:06 +0000

    update

---
+++ prog52a.py
@@ -0,0 +1,11 @@
+length = float(input("Please enter length: "))
+width = float(input("Please enter width: "))
+
+area = length * width
+perim = 2 * length + 2 * width
+
+print ("\nThe length is", length)
+print ("The width is", width)
+
+print("\nThe area is", area)
+print("The perimeter is", perim)
