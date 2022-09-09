ec9b5f4e2925797a398e2653a47e9c22a6ff2b11
granja-c <112727067+granja-c@users.noreply.github.com>
2022-09-09 19:31:11 +0000

    update

---
+++ prog54b.py
@@ -0,0 +1,10 @@
+first = float(input("First: "))
+second = float(input("Second: "))
+third = float(input("Third:"))
+fourth = float(input("Fourth:"))
+
+sum = first + second + third + fourth
+average = (first + second + third + fourth) / 4
+
+print("The sum of the four numbers is ", sum)
+print("The average of the four numbers is ", average)
